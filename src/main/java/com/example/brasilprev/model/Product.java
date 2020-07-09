6411c84c55c1168f2ff1b55824f7f64f77693554
jr-desenv <31509776+jr-desenv@users.noreply.github.com>
2020-07-09 13:46:50 -0300

    repository orders and products class

--- src/main/java/com/example/brasilprev/model/Product.java
+++ src/main/java/com/example/brasilprev/model/Product.java
@@ -8,4 +8,6 @@ public class Product {
 	
 	private String price;
 
+	
+	
 }
--- src/main/java/com/example/brasilprev/resource/ProductRepository.java
+++ src/main/java/com/example/brasilprev/resource/ProductRepository.java
@@ -1,5 +1,13 @@
 package com.example.brasilprev.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/products")
 public class ProductRepository {
+	
+
 
 }
